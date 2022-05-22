5f462908557a70d52130316fa326bbf1ab742612
laura <you@example.com>
2022-05-22 15:12:53 +0200

    cambios index.

---
+++ beyonce web/index.js
@@ -0,0 +1,40 @@
+/*una aplicacion que pida la edad mediante un prompt
+si la edad es amyor o igual a 18 el programa responde con 
+puedes pasar.si eres menor,responde con no puedes pasar.
+larespuesta se escribira en la pagina web
+// caso de exito
+*/
+let edad = prompt("Que edad tienes?");
+while = prompt (edad>=18){
+document.write("Puedes pasar");
+} else{
+        document.write("No puedes pasar");
+}
+
+
+//caso de error;
+//que me venga algo que no sea un numero
+if(isNaN(edad)){("Introduce un numero valido");
+
+}
+let numero = parseInt(prompt("lo que venga");
+
+// si viene un numero =1,2,3 o 16
+
+
+let edadConvertidaNumero=parseInt(edad);
+//console.log(edadConvertidaNumero);
+if(isNaNedadConvertidaNumero){
+    edad = prompt("Introduce un numero valido");
+}
+
+console.log(edad);
+document.write(edad);
+
+
+if(edadConvertidaNumero>=18){
+document.write ("Puedes pasar");
+} else{ 
+    document.write("No puedes pasar");
+}
+
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Tu código va aquí: 
+let edad =14
+if (edad <= 14) {
+    console.log("Es un menor de edad lo sentimos no puede entrar al concierto.");
+  } else {
+    console.log("esMayordeedad");
+  }
+
+if (edad <14&17) {("pueden entrar  sólo acompañadas de una persona mayor de 18");
+
+} else {("eresMayordeedad")
+    
+}
+
+ edad = 15;
+
+if (edad < 15) {
+  console.log(".Es un menor de edad lo sentimos no puede entrar al concierto.");
+} else if (edad > 17 && edad >= 21) {
+  console.log("eresMayordeedad");
+} else {("pueden entrar  sólo acompañadas de una persona mayor de 18")
+  console.log("true");
+}
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  }
+  letedad=16
+   nombre="Peter"
+if(edad>16){
+    console.log("Peter no recibe el ticket")
+}
